4bf3247bbc27e9f16719e39e2d2bf32c2584ba7a
szszss <772619266@qq.com>
2026-02-13 23:11:07 +0800

    Track file in debugging

---
+++ Source/YaOpt/Patches/Debugging/MultiTargets_PoolReplace.cs
@@ -0,0 +1,38 @@
+using System.Collections.Generic;
+using System.Reflection;
+using HarmonyLib;
+using Verse.AI;
+using YaOpt.Helpers;
+
+namespace YaOpt.Patches.Debugging
+{
+	[HarmonyPatch]
+	internal static class MultiTargets_ReservationManager
+	{
+		static IEnumerable<MethodBase> TargetMethods()
+		{
+			yield return AccessTools.Method(typeof(ReservationManager), nameof(ReservationManager.Reserve));
+			yield return AccessTools.Method(typeof(ReservationManager), nameof(ReservationManager.Release));
+			yield return AccessTools.Method(typeof(ReservationManager), nameof(ReservationManager.ReleaseAllForTarget));
+			yield return AccessTools.Method(typeof(ReservationManager), nameof(ReservationManager.ReleaseClaimedBy));
+			yield return AccessTools.Method(typeof(ReservationManager), nameof(ReservationManager.ReleaseAllClaimedBy));
+		}
+
+		static bool Prepare()
+		{
+			return YaOptGlobal.Settings.OptParallelJobGiver.Enabled;
+		}
+
+		static void Prefix()
+		{
+			if (ParallelJobGiver.WorkerThreadsRunning)
+			{
+				YaOptMod.Error("A Reservation operation was detected during the execution of " +
+							   "the ParallelJobGiver, which is strictly prohibited. " +
+							   "You can analyze the call stack to determine which WorkerGiver " +
+							   "performed this operation. " +
+							   "Please report this compatibility issue to the developers.");
+			}
+		}
+	}
+}
