9b4f97cc644d13d40f157f9cb3e960bb151b9917
szszss <772619266@qq.com>
2026-02-15 22:10:27 +0800

    Refactor main class

---
+++ Source/YaOpt/NativeLoader.cs
@@ -0,0 +1,96 @@
+using System;
+using System.IO;
+using System.Reflection;
+using Unity.Burst;
+using Verse;
+
+namespace YaOpt
+{
+	public static class NativeLoader
+	{
+		public static void LoadLibraries(Assembly modAssembly)
+		{
+			var dllDirectory = Path.GetDirectoryName(modAssembly.Location);
+			if (string.IsNullOrEmpty(dllDirectory))
+			{
+				YaOptMod.Error("Cannot get the location of mod dll. Some functions will not work.");
+				return;
+			}
+
+			string nativeDllName = null;
+			string initerName = null;
+			string burstDllName = null;
+
+			if (Environment.Is64BitProcess && Environment.Is64BitOperatingSystem)
+			{
+				if (Environment.OSVersion.Platform == PlatformID.Win32NT)
+				{
+					YaOptGlobal.IsWindows = true;
+					nativeDllName = "YaOpt.Native.Win64.dll";
+					initerName = "YaOpt.Native.Win64.Initer";
+					burstDllName = "yaopt_burst_win64.dll";
+				}
+				// Future: Add Linux/Mac support here
+				// else if (Environment.OSVersion.Platform == PlatformID.Unix) { ... }
+			}
+
+			if (nativeDllName != null)
+			{
+				LoadNativeLibrary(dllDirectory, nativeDllName, initerName);
+				
+				if (burstDllName != null)
+				{
+					LoadBurstLibrary(dllDirectory, burstDllName);
+				}
+			}
+			else
+			{
+				YaOptMod.Warning($"Some functions only work on 64Bit OS, current OS: {Environment.OSVersion}");
+			}
+
+			YaOptGlobal.IsLibraryLoaded = true;
+		}
+
+		private static void LoadNativeLibrary(string directory, string dllName, string initerType)
+		{
+			try
+			{
+				var dllPath = Path.GetFullPath(Path.Combine(directory, dllName));
+				YaOptMod.Debug($"Load Native library from {dllPath}");
+				var dll = Assembly.LoadFile(dllPath);
+				var type = dll.GetType(initerType, true);
+				// ReSharper disable once PossibleNullReferenceException
+				type.GetMethod("Init").Invoke(null, null);
+			}
+			catch (Exception e)
+			{
+				YaOptMod.Error($"Error when load {dllName}. Trampoline will be unavailable\n {e}");
+			}
+		}
+
+		private static void LoadBurstLibrary(string directory, string dllName)
+		{
+			try
+			{
+				var dllPath = Path.GetFullPath(Path.Combine(directory, "..\\Burst", dllName));
+				if (!File.Exists(dllPath))
+				{
+					throw new FileNotFoundException("Cannot find Burst library", dllPath);
+				}
+				YaOptMod.Debug($"Load Burst library from {dllPath}");
+				if (BurstRuntime.LoadAdditionalLibrary(dllPath))
+				{
+					YaOptGlobal.IsBurstAvailable = true;
+				}
+				else
+				{
+					YaOptMod.Error($"Failed to load Burst library from {dllPath}. Any features that require Burst will not work.");
+				}
+			}
+			catch (Exception e)
+			{
+				YaOptMod.Error($"Error when load Burst library.\n {e}");
+			}
+		}
+	}
+}
--- Source/YaOpt/YaOptMod.cs
+++ Source/YaOpt/YaOptMod.cs
@@ -1,9 +1,6 @@
 using HarmonyLib;
-using System;
 using System.Collections.Generic;
-using System.IO;
 using System.Reflection;
-using Unity.Burst;
 using UnityEngine;
 using Verse;
 using YaOpt.Helpers;
@@ -28,86 +25,36 @@ namespace YaOpt
 		{
 			Instance = this;
 			YaOptGlobal.IsMultiplayer = YaOptGlobal.HasMod("rwmt.Multiplayer");
-			SubMods.AddRange(YaOptSubMod.LoadAll());
-			YaOptSubMod.PreInitAll(SubMods);
+
+			InitSubMods();
+
 			Settings = GetSettings<YaOptSettings>();
 			Log("Now loading...");
-			var dllDirectory = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
-			if (string.IsNullOrEmpty(dllDirectory))
-			{
-				Error("Cannot get the location of mod dll. Some functions will not work.");
-			}
-			string nativeDllName = null;
-			string initerName = null;
-			string burstDllName = null;
 
-			if (Environment.Is64BitProcess && Environment.Is64BitOperatingSystem)
-			{
-				if (Environment.OSVersion.Platform == PlatformID.Win32NT)
-				{
-					YaOptGlobal.IsWindows = true;
-					nativeDllName = "YaOpt.Native.Win64.dll";
-					initerName = "YaOpt.Native.Win64.Initer";
-					burstDllName = "yaopt_burst_win64.dll";
-				}
-				// else if (Environment.OSVersion.Platform == PlatformID.Unix) { ... }
-			}
-			
-			if (nativeDllName != null)
-			{
-				if (!string.IsNullOrEmpty(dllDirectory))
-				{
-					try
-					{
-						var nativeDll = Path.GetFullPath(Path.Combine(dllDirectory, nativeDllName));
-						Debug($"Load Native library from {nativeDll}");
-						var dll = Assembly.LoadFile(nativeDll);
-						var type = dll.GetType(initerName, true);
-						// ReSharper disable once PossibleNullReferenceException
-						type.GetMethod("Init").Invoke(null, null);
-					}
-					catch (Exception e)
-					{
-						Error($"Error when load {nativeDllName}. Trampoline will be unavailable\n {e}");
-					}
-					try
-					{
-						if (burstDllName != null)
-						{
-							var burstDll = Path.GetFullPath(Path.Combine(dllDirectory, "..\\Burst\\" + burstDllName));
-							if (!File.Exists(burstDll))
-							{
-								throw new FileNotFoundException("Cannot find Burst library", burstDll);
-							}
-							Debug($"Load Burst library from {burstDll}");
-							if (BurstRuntime.LoadAdditionalLibrary(burstDll))
-							{
-								YaOptGlobal.IsBurstAvailable = true;
-							}
-							else
-							{
-								YaOptMod.Error($"Failed to load Burst library from {burstDll}. " +
-											   "Any features that require Burst will not work.");
-							}
-						}
-					}
-					catch (Exception e)
-					{
-						Error($"Error when load Burst library.\n {e}");
-					}
-				}
-			}
-			else
-			{
-				Warning($"Some functions only work on 64Bit OS, current OS: {Environment.OSVersion}");
-			}
-			YaOptGlobal.IsLibraryLoaded = true;
+			NativeLoader.LoadLibraries(Assembly.GetExecutingAssembly());
+
+			ApplySettings();
+			ApplyPatches();
+		}
+
+		private void InitSubMods()
+		{
+			SubMods.AddRange(YaOptSubMod.LoadAll());
+			YaOptSubMod.PreInitAll(SubMods);
+		}
+
+		private void ApplySettings()
+		{
 			Settings.ValidateOptions(true);
 			YaOptSubMod.InitAll(SubMods);
 
 #if DEBUG
 			HarmonyLib.Harmony.DEBUG = true;
 #endif
+		}
+
+		private void ApplyPatches()
+		{
 			if (Settings.OptLazyTextureLoad.Enabled)
 			{
 				ContentManager.Init();
