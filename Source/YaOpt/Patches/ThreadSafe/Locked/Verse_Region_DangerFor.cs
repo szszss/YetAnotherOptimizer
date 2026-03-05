a29c9360fc850ad0852c8fb0063ae5e9ea129f96
szszss <772619266@qq.com>
2026-03-06 05:17:20 +0800

    Fix Region.DangerFor

--- Source/YaOpt/Patches/ThreadSafe/Locked/Verse_Region_DangerFor.cs
+++ Source/YaOpt/Patches/ThreadSafe/Locked/Verse_Region_DangerFor.cs
@@ -19,20 +19,24 @@ namespace YaOpt.Patches.ThreadSafe.Locked
 		{
 			const int LOCAL_RANGE = 2;
 			var localIsMainThread = generator.DeclareLocal(typeof(bool));
+			var labelIfEnd = generator.DefineLabel();
 			var prepareEmitLockCheck = false;
 			var skip = false;
+			var shouldEmitLabel = false;
 			var counter = 0;
 			Label targetLabel = default;
-			// var isMainThread = YaOptGlobal.IsInMainThread;
+			// Step 1: var isMainThread = YaOptGlobal.IsInMainThread;
 			yield return CodeInstruction.Call(typeof(YaOptGlobal), "get_IsInMainThread");
 			yield return CodeInstruction.StoreLocal(localIsMainThread.LocalIndex);
 			foreach (var instruction in instructions)
 			{
+				// Step 2, 4:
 				if (instruction.Calls("get_ProgramState"))
 				{
 					prepareEmitLockCheck = true;
 					counter++;
 				}
+				// Step 8:
 				else if (instruction.Calls("set_Item"))
 				{
 					// don't write cachedDangers if current thread is not the main thread
@@ -48,25 +52,37 @@ namespace YaOpt.Patches.ThreadSafe.Locked
 					yield return new CodeInstruction(OpCodes.Nop).WithLabels(labelIsMainThreadEnd);
 					continue;
 				}
+				// Step 6:
 				else if (skip && instruction.labels.Contains(targetLabel))
 				{
 					skip = false;
+					shouldEmitLabel = true;
 				}
 
+				// Step 7:
+				if (shouldEmitLabel && instruction.opcode == OpCodes.Ldloca_S)
+				{
+					shouldEmitLabel = false;
+					instruction.WithLabels(labelIfEnd);
+				}
 				if (!skip)
+				{
 					yield return instruction;
+				}
 
 				if (prepareEmitLockCheck && instruction.opcode == OpCodes.Bne_Un_S)
 				{
 					prepareEmitLockCheck = false;
 					if (counter == 1) // cachedDangers block
 					{
+						// Step 3:
 						// don't read cachedDangers if current thread is not the main thread
 						yield return CodeInstruction.LoadLocal(localIsMainThread.LocalIndex);
 						yield return new CodeInstruction(OpCodes.Brfalse_S, instruction.operand);
 					}
 					else if (counter == 2) // cachedSafeTemperatureRanges block
 					{
+						// Step 5:
 						// floatRange = TemperatureHelper.GetSafeTemperatureRange(pawn);
 						// skip other code
 						yield return CodeInstruction.LoadArgument(1);
@@ -74,6 +90,7 @@ namespace YaOpt.Patches.ThreadSafe.Locked
 							typeof(RegionDangerHelper),
 							nameof(RegionDangerHelper.GetSafeTemperatureRange));
 						yield return CodeInstruction.StoreLocal(LOCAL_RANGE);
+						yield return new CodeInstruction(OpCodes.Br_S, labelIfEnd);
 						targetLabel = (Label)instruction.operand;
 						skip = true;
 					}
