b8c8809e259ad732c84a4b01bbd0dc4fe020d2d0
szszss <772619266@qq.com>
2026-04-28 03:37:20 +0800

    Fix missilegirl

--- Source/YaOpt/Patches/Compatibility/MissileGirl/MultiTargets_ComfortableTemperatureRange.cs
+++ Source/YaOpt/Patches/Compatibility/MissileGirl/MultiTargets_ComfortableTemperatureRange.cs
@@ -1,6 +1,7 @@
 using HarmonyLib;
 using System.Collections.Generic;
 using Verse;
+using YaOpt.Helpers;
 using YaOpt.Helpers.ThirdParty;
 
 namespace YaOpt.Patches.Compatibility.MissileGirl
@@ -10,12 +11,18 @@ namespace YaOpt.Patches.Compatibility.MissileGirl
 	{
 		private static UnfairRwLock _rwLock = new UnfairRwLock();
 
+		private static AccessTools.FieldRef<int> _lastTickFieldRef;
+
+		private static AccessTools.FieldRef<Dictionary<int, FloatRange>> _tempCacheFieldRef;
+
+		private static bool _callbackRegistered = false;
+
 		static void Patch(Harmony harmony)
 		{
-			if (YaOptGlobal.NeedThreadSafe && YaOptGlobal.HasType("RocketMan.Optimizations.GenTemperature_Patch"))
+			if (YaOptGlobal.NeedThreadSafe && YaOptGlobal.HasType("MissileGirl.Optimizations.GenTemperature_Patch"))
 			{
 				var type = AccessTools.TypeByName(
-					"RocketMan.Optimizations.GenTemperature_Patch");
+					"MissileGirl.Optimizations.GenTemperature_Patch");
 				var helperType = typeof(MultiTargets_ComfortableTemperatureRange);
 				harmony.Patch(AccessTools.Method(type, "Prefix"),
 					prefix: new HarmonyMethod(helperType, nameof(EnterReadLock)),
@@ -23,22 +30,38 @@ namespace YaOpt.Patches.Compatibility.MissileGirl
 				harmony.Patch(AccessTools.Method(type, "Postfix"),
 					prefix: new HarmonyMethod(helperType, nameof(EnterWriteLock)),
 					finalizer: new HarmonyMethod(helperType, nameof(ExitWriteLock)));
+
+				if (!_callbackRegistered)
+				{
+					_callbackRegistered = true;
+					_lastTickFieldRef = AccessTools.StaticFieldRefAccess<int>(
+						AccessTools.Field(type, "LastTick"));
+					_tempCacheFieldRef = AccessTools.StaticFieldRefAccess<Dictionary<int, FloatRange>>(
+						AccessTools.Field(type, "tempCache"));
+					UpdateCallbackHelper.RegisterPreTickCallback(PreTick);
+				}
 			}
 		}
 
-		public static void EnterReadLock(out bool __state)
+		private static void PreTick(int tick)
+		{
+			_lastTickFieldRef() = tick;
+			_tempCacheFieldRef().Clear();
+		}
+
+		private static void EnterReadLock(out bool __state)
 		{
 			__state = true;
 			_rwLock.EnterReadLock();
 		}
 
-		public static void ExitReadLock(bool __state)
+		private static void ExitReadLock(bool __state)
 		{
 			if (__state)
 				_rwLock.ExitReadLock();
 		}
 
-		public static void EnterWriteLock(Pawn p, Dictionary<int, FloatRange> ___tempCache,
+		private static void EnterWriteLock(Pawn p, Dictionary<int, FloatRange> ___tempCache,
 			out bool __state)
 		{
 			_rwLock.EnterReadLock();
@@ -58,7 +81,7 @@ namespace YaOpt.Patches.Compatibility.MissileGirl
 			_rwLock.EnterWriteLock();
 		}
 
-		public static void ExitWriteLock(bool __state)
+		private static void ExitWriteLock(bool __state)
 		{
 			if (__state)
 				_rwLock.ExitWriteLock();
