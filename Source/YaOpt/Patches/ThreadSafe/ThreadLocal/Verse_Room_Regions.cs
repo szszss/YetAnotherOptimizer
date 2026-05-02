aa6369cf8de8a80b4459cbee678f8f74f41f706b
szszss <772619266@qq.com>
2026-05-03 03:53:30 +0800

    fix: System.InvalidOperationException when a thread uses two Room.Regions simultaneously.

--- Source/YaOpt/Patches/ThreadSafe/ThreadLocal/Verse_Room_Regions.cs
+++ Source/YaOpt/Patches/ThreadSafe/ThreadLocal/Verse_Room_Regions.cs
@@ -1,12 +1,13 @@
 using HarmonyLib;
 using System.Collections.Generic;
+using System.Linq;
 using System.Reflection.Emit;
 using Verse;
+using YaOpt.Helpers;
 using YaOpt.Helpers.ThreadLocal;
 
 namespace YaOpt.Patches.ThreadSafe.ThreadLocal
 {
-	// TODO: This is not a robust fix. It fails when a thread uses two Room.Regions simultaneously.
 	[HarmonyPatch(typeof(Room))]
 	[HarmonyPatch(nameof(Room.Regions), MethodType.Getter)]
 	internal static class Verse_Room_Regions
@@ -16,9 +17,50 @@ namespace YaOpt.Patches.ThreadSafe.ThreadLocal
 			return YaOptGlobal.NeedThreadSafe;
 		}
 
+		// Old implementation: It fails when a thread uses two Room.Regions simultaneously.
+		/*
 		static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions, ILGenerator generator)
 		{
 			return ThreadLocalHelper.ThreadLocalTranspiler(instructions, generator, "tmpRegions");
+		}
+		*/
+
+		// It's still not truly robust; it fails when other threads attempt to modify Room.Regions,
+		// but it's better than the old implementation.
+		static bool Prefix(List<Region> ___tmpRegions, List<District> ___districts, ref List<Region> __result)
+		{
+			__result = ___tmpRegions;
+			List<Region> tmpList = null;
+			try
+			{
+				tmpList = ConcurrentPool<List<Region>>.Get();
+				tmpList.Clear();
+				for (int i = 0; i < ___districts.Count; i++)
+				{
+					var regions = ___districts[i].Regions;
+					for (var j = 0; j < regions.Count; j++)
+					{
+						tmpList.Add(regions[j]);
+					}
+				}
+				lock (___tmpRegions)
+				{
+					if (!___tmpRegions.SequenceEqual(tmpList))
+					{
+						___tmpRegions.Clear();
+						___tmpRegions.AddRange(tmpList);
+					}
+				}
+			}
+			finally
+			{
+				if (tmpList != null)
+				{
+					tmpList.Clear();
+					ConcurrentPool<List<Region>>.Return(tmpList);
+				}
+			}
+			return false;
 		}
 	}
 }
