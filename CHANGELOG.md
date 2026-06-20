c0143f8cf7020fef3955a3753d71b47e34d9031d
szszss <772619266@qq.com>
2026-06-21 04:43:06 +0800

    fix: Dubs Bad Hygiene may throw System.InvalidOperationException shortly after loading a save.

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,6 +1,7 @@
 * Compatibility: "Faster GetComp methods replacement" of Performance Optimizer.
 * Compatibility: "DoBillOptimization.TryFindBestIngredientsInSet_NoMixHelper" and "DoBillPrepatches.TryFindBestIngredientsHelper_InnerDelegate" of Performance Fish.
 * Fix: Attempting to fix the System.ArgumentOutOfRangeException in Build From Inventory - Continued (Unable to verify, BFI does not work in any case in my environment).
+* Fix: Dubs Bad Hygiene may throw System.InvalidOperationException shortly after loading a save.
 * Fix: Thread-safe for GenConstruct.anGetResources_NewTemp.
 * Refactor: Adjust the patching priority and error detection of Reachability thread safety patches.
 
--- Source/YaOpt/Helpers/CacheWarmer.cs
+++ Source/YaOpt/Helpers/CacheWarmer.cs
@@ -1,4 +1,6 @@
+using HarmonyLib;
 using System;
+using System.Collections.Generic;
 using Verse;
 
 namespace YaOpt.Helpers
@@ -7,13 +9,46 @@ namespace YaOpt.Helpers
 	{
 		public static void PostInit()
 		{
+			WarmPawnRenderNodeWorker();
+			WarmDubsBadHygiene();
+		}
+
+		private static void WarmPawnRenderNodeWorker()
+		{
 			var count = 0;
 			foreach (Type type in typeof(PawnRenderNodeWorker).AllSubclassesNonAbstract())
 			{
 				GenWorker<PawnRenderNodeWorker>.Get(type);
 				count++;
 			}
-			YaOptMod.Debug($"CacheWarmer PostInit: Warm {count} PawnRenderNodeWorkers");
+			YaOptMod.Debug($"CacheWarmer: Warm {count} PawnRenderNodeWorkers");
+		}
+
+		private static void WarmDubsBadHygiene()
+		{
+			if (!YaOptGlobal.HasMod("dubwise.dubsbadhygiene"))
+				return;
+
+			var typeCompWaterFillable = AccessTools.TypeByName("DubsBadHygiene.CompWaterFillable");
+			var typeBuildingWashBucket = AccessTools.TypeByName("DubsBadHygiene.Building_washbucket");
+			var fieldCachedRefillableDefs = AccessTools.Field(
+				AccessTools.TypeByName("DubsBadHygiene.WorkGiver_RefillWater"),
+				"cachedRefillableDefs"
+			);
+			var cachedRefillableDefs = new List<ThingDef>();
+			foreach (ThingDef thingDef in DefDatabase<ThingDef>.AllDefsListForReading)
+			{
+				if (thingDef.HasComp(typeCompWaterFillable))
+				{
+					cachedRefillableDefs.Add(thingDef);
+				}
+				if (typeBuildingWashBucket.IsAssignableFrom(thingDef.thingClass))
+				{
+					cachedRefillableDefs.Add(thingDef);
+				}
+			}
+			fieldCachedRefillableDefs.SetValue(null, cachedRefillableDefs);
+			YaOptMod.Debug("CacheWarmer: Warm Dubs Bad Hygiene water storage cache");
 		}
 	}
 }
